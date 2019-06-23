6f32bfe236e87a9b582ef604c9d151d0f0321273
香取美貴子 <katorimikiko@katorimikikonomacbook-air.local>
2019-06-23 22:11:40 +0900

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by 香取美貴子 on 2019/06/23.
+//  Copyright © 2019 香取美貴子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
